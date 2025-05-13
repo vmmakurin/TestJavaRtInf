bf729d27dc4df96da435964d8a99c15b67b7ee1c
Valeriy Makurin <vmmakurin@mail.ru>
2025-05-13 11:56:25 +0300

    Add element frequency calculation to main method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        // Тест-кейс 1
+        int[] array1 = {1, 3, 4, 5, 1, 5, 4};
+        System.out.println("Test Case 1:");
+        printFrequencies(array1);
 
+        // Тест-кейс 2
+        int[] array2 = {2, 2, 2, 2, 2};
+        System.out.println("Test Case 2:");
+        printFrequencies(array2);
+
+        // Тест-кейс 3
+        int[] array3 = {};
+        System.out.println("Test Case 3:");
+        printFrequencies(array3);
+    }
+
+    public static void printFrequencies(int[] array) {
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int num : array) {
+            result.put(num, result.getOrDefault(num, 0) + 1);
+        }
+
+        if (result.isEmpty()) {
+            System.out.println("Пустой массив. Нет элементов для подсчёта.");
+        } else {
+            for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
+        }
+
+        System.out.println();
     }
 }
